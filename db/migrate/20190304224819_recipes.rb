e350c4d127447b2341cd2a8ffb01051b31a97174
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-03-04 22:55:20 +0000

    Automatically backed up by Learn

--- db/migrate/20190304224819_recipes.rb
+++ db/migrate/20190304224819_recipes.rb
@@ -1,8 +1,9 @@
-class CreateRecipes < ActiveRecord::Migration[5.1]
+class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :ingredients 
+      t.string :ingredients
+      t.string :cook_time
     end
   end
 end
